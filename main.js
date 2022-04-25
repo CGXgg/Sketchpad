1c7a28c70a14e32cbbdbad0e3bf8e89e85ed3e8f
CGX <601051903@qq.com>
2022-04-25 23:31:32 +0800

    feat:添加画笔三种颜色

--- main.js
+++ main.js
@@ -1,1 +1,139 @@
-dsd;
+var yyy = document.getElementById('xxx');
+var context = yyy.getContext('2d');
+autoSetCanvasSize(yyy);
+
+listenToUser(yyy);
+
+var eraserEnabled = false;
+pen.onclick = function () {
+  console.log(2);
+  eraserEnabled = false;
+  pen.classList.add('active');
+  eraser.classList.remove('active');
+};
+
+eraser.onclick = function () {
+  eraserEnabled = true;
+  eraser.classList.add('active');
+  pen.classList.remove('active');
+};
+red.onclick = function () {
+  context.fillStyle = 'red';
+  context.strokeStyle = 'red';
+  red.classList.add('active');
+  green.classList.remove('active');
+  blue.classList.remove('active');
+};
+green.onclick = function () {
+  context.fillStyle = 'green';
+  context.strokeStyle = 'green  ';
+  red.classList.remove('active');
+  green.classList.add('active');
+  blue.classList.remove('active');
+};
+blue.onclick = function () {
+  context.fillStyle = 'blue';
+  context.strokeStyle = 'blue';
+  red.classList.remove('active');
+  green.classList.remove('active');
+  blue.classList.add('active');
+};
+/*****/
+function autoSetCanvasSize(canvas) {
+  setCanvasSize();
+  window.onresize = function () {
+    setCanvasSize();
+  };
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth;
+    var pageHeight = document.documentElement.clientHeight;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+  }
+}
+function drawCircle(x, y, radius) {
+  context.beginPath();
+  context.arc(x, y, radius, 0, Math.PI * 2);
+  context.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath();
+  context.moveTo(x1, y1); //起点
+  context.lineWidth = 5;
+  context.lineTo(x2, y2); //终点
+  context.stroke(); //
+  context.closePath();
+}
+
+var using = false;
+function listenToUser(canvas) {
+  var lastPoint = { x: undefined, y: undefined };
+
+  if (document.body.ontouchstart !== undefined) {
+    //触屏设备
+
+    canvas.ontouchstart = function (aaa) {
+      console.log('开始');
+      var x = aaa.touches[0].clientX;
+      var y = aaa.touches[0].clientY;
+      using = true;
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10);
+      } else {
+        lastPoint = { x: x, y: y };
+      }
+    };
+    canvas.ontouchmove = function (aaa) {
+      console.log('中间');
+      var x = aaa.touches[0].clientX;
+      var y = aaa.touches[0].clientY;
+      if (!using) {
+        return;
+      }
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10);
+      } else {
+        var newPoint = { x: x, y: y };
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+      }
+      lastPoint = newPoint;
+    };
+
+    canvas.ontouchend = function () {
+      console.log('松开');
+      using = false;
+    };
+  } else {
+    //非触屏设备
+    canvas.onmousedown = function (aaa) {
+      console.log(2222);
+      var x = aaa.clientX;
+      var y = aaa.clientY;
+      using = true;
+      if (eraserEnabled) {
+        console.log('eraserEnabled: ', eraserEnabled);
+        context.clearRect(x - 5, y - 5, 10, 10);
+      } else {
+        lastPoint = { x: x, y: y };
+      }
+    };
+    canvas.onmousemove = function (aaa) {
+      var x = aaa.clientX;
+      var y = aaa.clientY;
+      if (!using) {
+        return;
+      }
+      if (eraserEnabled) {
+        context.clearRect(x - 5, y - 5, 10, 10);
+      } else {
+        var newPoint = { x: x, y: y };
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+      }
+      lastPoint = newPoint;
+    };
+    canvas.onmouseup = function (aaa) {
+      using = false;
+    };
+  }
+}
