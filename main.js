c1a4686b3f0e1fca5deec02de34996e85443dd80
CGX <601051903@qq.com>
2022-04-26 18:52:56 +0800

    feat:添加下载

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
+var lineWidth = 5;
+
 autoSetCanvasSize(yyy);
 
 listenToUser(yyy);
@@ -17,6 +19,21 @@ eraser.onclick = function () {
   eraser.classList.add('active');
   pen.classList.remove('active');
 };
+
+clear.onclick = function () {
+  context.clearRect(0, 0, yyy.width, yyy.height);
+};
+download.onclick = function () {
+  var url = yyy.toDataURL('image/png');
+  console.log(url);
+  var a = document.createElement('a');
+  document.body.appendChild(a);
+  a.href = url;
+  a.download = '我的画';
+  a.target = '_blank';
+  a.click();
+};
+
 red.onclick = function () {
   context.fillStyle = 'red';
   context.strokeStyle = 'red';
@@ -37,6 +54,13 @@ blue.onclick = function () {
   red.classList.remove('active');
   green.classList.remove('active');
   blue.classList.add('active');
+};
+
+thin.onclick = function () {
+  lineWidth = 5;
+};
+thick.onclick = function () {
+  lineWidth = 10;
 };
 /*****/
 function autoSetCanvasSize(canvas) {
@@ -60,7 +84,7 @@ function drawCircle(x, y, radius) {
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
   context.moveTo(x1, y1); //起点
-  context.lineWidth = 5;
+  context.lineWidth = lineWidth;
   context.lineTo(x2, y2); //终点
   context.stroke(); //
   context.closePath();
